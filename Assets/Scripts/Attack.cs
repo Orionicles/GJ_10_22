425e7688ae6f30dd4758792c82d040bddce5ec3a
austin81215 <aumaxx04@gmail.com>
2022-10-22 16:19:43 -0600

    added attack superclass for boss + test scene

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private GameManager manager;
+
+    public void Awake()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "PlayerTest")
+        {
+            manager.PlayerTakeDamage();
+            //Debug.Log("here");
+        }
+    }
+
+    //public void Update()
+    //{
+    //    transform.Translate(.05f, 0f, 0f);
+    //}
+}
